2a8cee0fa517c708b51ed24a41fe64c002efc58d
Daniel Mannix <mannix.daniel@gmail.com>
2022-04-09 20:59:43 +0100

    Start of Project
    Set up Dependencies using NPM
    Start react project
    Tested modules loading as intended

---
+++ todo-app/src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+ class SecondComponent extends Component{
+    render() {
+      return(
+        <div className="secondCompnent">
+          Test the second compnent
+        </div>
+      );
+    }
+  }
+  
+  export default SecondComponent;
