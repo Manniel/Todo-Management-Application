3c29040f1bceb0e9a038d34433c86330f1cecbe9
Daniel Mannix <mannix.daniel@gmail.com>
2022-05-09 03:02:21 +0100

    Login Event
    Added onclick login event to check functionality using hardcoded credentials

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -17,10 +17,14 @@ class LoginComponent extends Component{
 
         this.state = {
             username: "User Name",
-            password: ""
+            password: "",
+            hasLoginFailed: false,
+            showSuccessfulMessage: false
+
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.loginClicked = this.loginClicked.bind(this);
 
     }
 
@@ -31,17 +35,45 @@ class LoginComponent extends Component{
             [event.target.name]: event.target.value
         })
     }
+
+    loginClicked(event){
+        if(this.state.username==='Daniel' && this.state.password==='password'){
+            this.setState({
+                showSuccessfulMessage:true,
+                hasLoginFailed:false
+            })
+        } else {
+            this.setState({
+                showSuccessfulMessage:false,
+                hasLoginFailed:true
+            })
+        }
+        console.log("clicked");
+        
+    }
     
     
 
     render(){
         return(
             <div>
+                <ShowCredentialsMessage hasLoginFailed={this.state.hasLoginFailed} showSuccessfulMessage={this.state.showSuccessfulMessage}/>
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}></input>
                 Password: <input type="password" name="password" vale={this.state.password} onChange={this.handleChange}></input>
-                <button className="Login">Login</button>
+                <button className="Login" onClick={this.loginClicked}>Login</button>
             </div>
         )
+    }
+
+}
+
+function ShowCredentialsMessage(props){
+    if(props.hasLoginFailed){
+        return <div>Invalid Login</div>
+    } else if(props.showSuccessfulMessage){
+        return <div>Successful Login</div>
+    } else {
+        return null;
     }
 }
 
